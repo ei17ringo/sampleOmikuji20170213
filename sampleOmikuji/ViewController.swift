fe3b31f7f72dcb0d4c02e330c02d7d2aca42debb
ei17ringo <ei17ringo@gmail.com>
2017-03-11 12:06:20 +0800

    init commit

--- sampleOmikuji/ViewController.swift
+++ sampleOmikuji/ViewController.swift
@@ -14,6 +14,56 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //占いボタンが押されたとき
+    @IBAction func tapUranai(_ sender: UIButton) {
+        
+        //占いの結果を配列で用意する
+        var omikuji = ["大吉","吉","中吉","小吉","末吉","凶","大凶"]
+        
+        //占いの結果をランダムに選ぶための数字を作成
+        let r = Int(arc4random()) % omikuji.count
+        
+        //デバッグエリアに運勢を表示
+        print("今日の運勢:\(omikuji[r])")
+        
+        //アラートを作る
+        let alertController = UIAlertController(title: "今日の運勢", message: omikuji[r], preferredStyle: .alert)
+        
+        //OKボタンを追加
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in self.kekka(resultNumber: r)}))
+        
+        //アラートを表示
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    //関数
+    //OKボタンが押されたとき発動
+    // kekka:関数名
+    // resultNumber:引数（処理をするために必要な情報）
+    func kekka(resultNumber:Int){
+        // resultNumber = おみくじをランダムに決定する数字
+        print("おみくじ番号\(resultNumber)")
+        
+        // TODO:OKが押された時に、各運勢に紐付いた画像を表示しましょう
+        switch resultNumber {
+        case 0:
+            //大吉
+            print("大吉")
+        case 1:
+            print("吉")
+            
+        case 2:
+            print("中吉")
+            
+        case 3:
+            print("小吉")
+            
+        default:
+            print("その他")
+        }
+    
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
